f848231b3b0e1f911adb785755fc4db1180e3905
Darrion Mills <dmills1994@gmail.com>
2021-04-19 00:50:08 -0400

    Create new note feature added

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hive/hive.dart';
-import 'package:mobile_notes_app/data/data_sources/hive_db.dart';
 import 'package:mobile_notes_app/data/models/note.dart';
 import 'package:mobile_notes_app/data/models/tag.dart';
 import 'package:mobile_notes_app/data/models/task.dart';
 import 'package:mobile_notes_app/notes/bloc/notes_bloc.dart';
 import 'package:mobile_notes_app/ui/note_card.dart';
+import 'package:mobile_notes_app/ui/note_page.dart';
 import 'package:mobile_notes_app/ui/themes.dart';
 
 class App extends StatelessWidget {
@@ -14,17 +13,12 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<NotesBloc>(
-      create: (context) => NotesBloc(
-        repository: HiveDatabase(
-          notesBox: Hive.box('notesBox'),
-        ),
-      ),
-      child: MaterialApp(
-        themeMode: ThemeMode.dark,
-        darkTheme: Themes.darkTheme,
-        home: SafeArea(
-          child: Scaffold(
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      darkTheme: Themes.darkTheme,
+      home: SafeArea(
+        child: Builder(
+          builder: (context) => Scaffold(
             backgroundColor: Themes.noteColours['black'],
             bottomNavigationBar: const BottomAppBar(
               color: Themes.darkBackgroundColor,
@@ -33,7 +27,23 @@ class App extends StatelessWidget {
             // floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
             floatingActionButton: FloatingActionButton(
               backgroundColor: Themes.noteColours['blue'],
-              onPressed: null,
+              onPressed: () {
+                final newNote = Note(
+                  body: '',
+                  title: '',
+                  dateTime: DateTime.now(),
+                  id: '2',
+                );
+
+                context.read<NotesBloc>().add(NoteAdded(newNote));
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => NotePage(note: newNote),
+                  ),
+                );
+              },
               child: const Icon(Icons.add),
             ),
             body: Column(
@@ -59,29 +69,6 @@ class App extends StatelessWidget {
                       }
                       return Container();
                     },
-                  ),
-                ),
-                Builder(
-                  builder: (context) => TextButton(
-                    onPressed: () {
-                      context.read<NotesBloc>().add(
-                            NoteUpdated(
-                              Note(
-                                  title: 'Similarity',
-                                  body:
-                                      '''Elements that share similar properties are perceived as more related.\n\nAny number of characteristics can be similar: colour, shape, size, texture, etc.''',
-                                  id: '1',
-                                  dateTime: DateTime.now(),
-                                  tags: const [
-                                    Tag(title: 'Design'),
-                                  ],
-                                  tasks: const [
-                                    Task(body: 'Test task', isCompleted: true),
-                                  ]),
-                            ),
-                          );
-                    },
-                    child: const Text('Press'),
                   ),
                 ),
               ],
